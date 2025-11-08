688acbb9f5bbf1dfc3ba495bcc64139c9e20aea2
jenssen10 <jenssenhonore@yahoo.com>
2025-11-08 14:39:12 -0500

    initial commit

---
+++ Footer.js
@@ -0,0 +1,20 @@
+return(<footer>
+<hr/>
+<menu>
+<h3>Social Media Links</h3>
+<li><a href="https://github.com/jenssen10/littlelemon.git">GitHub</a></li>
+<li><a href="https://www.linkedin.com/in/jenssen-honore-a3b158241/">LinkedIn</a></li>
+
+<h3>Contact Info</h3>
+<li><a href="Address.html">Address</a></li>
+<li><a href="Phone number.html">Phone Number</a></li>
+<li><a href="Email.html">Email</a></li>
+<h3>Doormat Navigation</h3>
+<li><a href="Home.html">Home</a></li>
+<li><a href="about.html">About</a></li>
+<li><a href="menu.html">Menu</a></li>
+<li><a href="reservations.html">Reservations</a></li>
+<li><a href="order online.html">Order Online</a></li>
+<li><a href="login.html">Login</a></li>
+</menu>
+        </footer>); 
---
+++ nav.js
@@ -0,0 +1,14 @@
+return(<nav><ul>
+    <li><a href="Home.html">Home</a>
+</li>
+    <li><a href="About.html">About</a>
+</li>
+    <li><a href="Menu.html">Menu</a>
+</li>
+    <li><a href="Reservations.html">Reservations</a>
+</li>
+    <li><a href="Order Online.html">Order Online</a>
+</li>
+    <li><a href="Login.html">Login</a>
+</li>
+    </ul></nav>); 
